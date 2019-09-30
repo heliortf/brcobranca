f7c6d2643ab54049060f8303e0b21d712488e156
heliortf <heliortf@gmail.com>
2019-09-30 11:28:42 -0300

    Update rghost.rb

--- lib/brcobranca/boleto/template/rghost.rb
+++ lib/brcobranca/boleto/template/rghost.rb
@@ -183,7 +183,7 @@ module Brcobranca
           doc.show "#{boleto.sacado} - #{boleto.sacado_documento.formata_documento}"
 
           move_more(doc, 0, -0.3)
-          doc.show "#{boleto.sacado_endereco}"
+          doc.text "#{boleto.sacado_endereco}"
           if boleto.demonstrativo
             doc.text_area boleto.demonstrativo, width: '18.5 cm', text_align: :left, x: "#{@x - 0.8} cm", y: "#{@y - 0.9} cm", row_height: '0.4 cm'
           end
